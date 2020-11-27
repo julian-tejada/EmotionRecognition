bd2eb9989808ef01d1c08a207096729297ce1a59
gabes <cardoso_49@hotmail.com>
2020-11-26 22:20:26 -0300

    Working release and Parsing
    The script is now ready to be properly executed and parsing has been added to enhance user experience.
    Command: python [filename] -l numbers, where numbers is a list of ints corresponding to the attributes desired.  For further options, use command: python [filename] -h. (Only heat map implemented so far)

--- scripts/panda_test.py
+++ scripts/panda_test.py
@@ -9,12 +9,12 @@ Created on Tue Oct 20 13:25:54 2020
 
 # from os import listdir
 # from os.path import isfile, join
+import argparse
 import numpy as np
 import pandas as pd
 from sklearn.svm import SVC
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, plot_confusion_matrix
 import pylab as pl
-from sklearn.metrics import plot_confusion_matrix
 import matplotlib.pyplot as plt
 
 # datasets tem tamanhos diferentes
@@ -41,6 +41,8 @@ facs:                       AU01_r to AU45_c
 
 # csv_files = [f for f in listdir('../csv') if isfile(join('../csv', f))]
 csv_files = ['../csv/anger.csv', '../csv/deboche.csv', '../csv/disgust.csv', '../csv/fear.csv', '../csv/happiness.csv', '../csv/neutral.csv', '../csv/sadness.csv', '../csv/surprise.csv']
+
+
 
 def create_lists(n):
 
@@ -78,12 +80,27 @@ def create_lists(n):
     29: ['AU01_r', 'AU45_c']                # facs
     }
 
+    strings = {
+    2: "gaze",
+    3: "eye_2D",
+    5: "eye_3D",
+    7: "head pose (no rotation)",
+    11: "head pose (w/ rotation)",
+    13: "face_2D",
+    17: "face_3D",
+    19: "poses_1",
+    23: "poses_2",
+    29: "facs"
+    }
+
     # Initialize empty list of labels, which will later be a DataFrame
 
     labels = []
+    selection = ""
 
     for i in aspects.keys():
         if n % i == 0:
+            selection += strings[i] + "; "
             start = intervals[i][0]
             end = intervals[i][1]
             for j in range(len(csv_files)):
@@ -101,28 +118,76 @@ def create_lists(n):
         labels.append(tempClass)
     labels = pd.concat(labels)
 
+    #print(selection)    
     
-    
-    return aspects, labels
+    return aspects, labels, selection
 
 # alinhar classes com dataframes em aspects
 
 def training_and_test(n):
-    aspects, labels = create_lists(n)
+    aspects, labels, selection = create_lists(n)
     dfs = [x for x in aspects.values()]
     full_df = pd.concat([labels, pd.concat(dfs, axis=1)], axis=1)
 
     sampled_set = full_df.sample(len(full_df))
     training_set = sampled_set[0 : int(len(sampled_set)*0.8)]
     test_set = sampled_set[len(training_set)+1 : len(sampled_set)]
-    #test_set = full_df[len(training_set)+1 : len(full_df)]
 
-    return sampled_set, training_set, test_set
+    print(selection)
+
+    return training_set, test_set, selection
 
 
+def classifier(n, k, c):
+    # Separate training and test data from the labels
+    training_set, test_set, selection = training_and_test(n)
+    training = training_set.iloc[:, 1:]
+    training_labels = training_set[["class"]]
 
+    test = test_set.iloc[:, 1:]
+    test_labels = test_set[["class"]]
+
+    npar_train = training.to_numpy()
+    npar_trainlabs = training_labels.to_numpy()
+
+    npar_predict = test.to_numpy()
+    npar_predictlabs = test_labels.to_numpy()
     
+    clf = SVC(kernel=k, C=c, probability=True)
+    classifier = clf.fit(npar_train, npar_trainlabs.flatten())
+    
+    predictions = clf.predict(npar_predict)
+    probabilities = clf.predict_proba(npar_predict)
+    
+    cm = confusion_matrix(test_labels, predictions, normalize="true")
+
+    pl.matshow(cm)
+    pl.title("Heat map of: " + selection)
+    #pl.colorbar()
+    pl.show()
+    
+    return cm
+    
+
+parser = argparse.ArgumentParser(description="Train a classifier to predict and classify emotions")
+
+# Positional (mandatory arguments)
+# parser.add_argument("n", type=int, help="int (or product of ints) of aspect(s) to be trained")
+parser.add_argument("-l", "--list", nargs='+', help="list of attributes", required=True)
+
+# Optional arguments
+parser.add_argument("-k", "--kernel", default='linear', help="set kernel of the SVC (default is set to linear)")
+parser.add_argument("-c", "--C", type=float, default=10, help="set C value of the SVC (default is set to 10)")
+
+args = parser.parse_args()
 
 
 #print(create_lists(2))
-print(training_and_test(19))
+#print(training_and_test(17))
+
+#classifier(args.n, args.kernel, args.C)
+
+for m in args.list:
+    m = int(m)
+    classifier(m, args.kernel, args.C)
+    
